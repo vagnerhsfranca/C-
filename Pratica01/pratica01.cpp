3e87f4b84f3c8e5c3099785ea0955b24163876c0
VagnerhsFranca <76712580+VagnerhsFranca@users.noreply.github.com>
2021-01-28 01:37:11 -0300

    Initial commit

---
+++ Pratica01/pratica01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Veiculo{
+	
+	private:
+		string nome;
+		
+	public:
+		Veiculo(const char *p){
+			this->nome = p;
+			cout << "O objeto " << nome << " foi criado" << endl;
+		}
+		
+		~Veiculo(){
+			cout << "O objeto " << nome << " foi destruído" << endl;	
+		}
+		
+};
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+ cout << "Primeira aplicação C++" <<endl;
+ 
+// Veiculo veiculo1("v1");
+//{
+//	Veiculo veiculo2("v2");
+//	{
+//		Veiculo veiculo3("v3");
+//	}
+//}
+	Veiculo *obj1 = new Veiculo("v1");
+	Veiculo *obj2 = new Veiculo("v2");
+	Veiculo *obj3 = new Veiculo("v3");
+	
+	delete obj3;
+	delete obj2;
+	delete obj1;
+
+  return 0;
+}
+
